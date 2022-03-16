8403cd56eced4fbf6b19755f224ce4be652ed541
Sital Pun <shitalmagar21@gmail.com>
2022-03-16 10:58:39 -0500

    Initialized first code base

---
+++ src/main/java/com/kuebiko/fakebank/FakebankApplication.java
@@ -0,0 +1,13 @@
+package com.kuebiko.fakebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuebiko/fakebank/FakebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuebiko.fakebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

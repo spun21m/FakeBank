99c906c6a5daaf4f0a1897cc403fdbd5de71da58
Sital Pun <shitalmagar21@gmail.com>
2022-03-20 18:20:30 -0500

    Account Information Model to create account added

---
+++ src/main/java/com/kuebiko/fakebank/models/AccountInformation.java
@@ -0,0 +1,67 @@
+package com.kuebiko.fakebank.models;
+
+public class AccountInformation {
+    private String firstName;
+    private String lastName;
+    private Integer ssn;
+    private String street;
+    private String city;
+    private Integer zipcode;
+    private String state;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(Integer ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(Integer zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
